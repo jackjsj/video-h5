b63bb9d33d11f0f297abe1e7023fc6331eb0c75f
jackjsj <jackjsj@foxmail.com>
2019-12-06 15:14:54 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [ 'plugin:vue/essential', '@vue/airbnb' ],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'linebreak-style': [ 'off', 'windows' ],
+		'no-param-reassign': [ 'error', { props: false } ],
+		'arrow-parens': [ 'error', 'always' ],
+		'operator-linebreak': [ 'off' ],
+		'import/extensions': [ 'off' ],
+		'object-curly-newline': [ 'off' ],
+		'global-require': [ 'off' ],
+		'arrow-parens': [ 'off' ],
+		'import/prefer-default-export': [ 'off' ],
+		'no-unused-expressions': [ 'off' ],
+		'guard-for-in': [ 'off' ],
+		'no-restricted-syntax': [ 'off' ]
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 };
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  outputDir: 'dist',
+  publicPath: './',
+  lintOnSave: true,
+  devServer: {
+    proxy: {
+      // '/spaceIngelligent': {
+      //   target: 'http://baota.qdaoliqi.cn:8080/',
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '^/spaceIngelligent': '/spaceIngelligent',
+      //   },
+      // },
+    },
+  },
+};
